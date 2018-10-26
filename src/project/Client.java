35b1d9b86f6390091aa9483155c165e106062a8f
campbellg4 <campbellg4@WIT45005734.wit.private>
2018-10-26 12:35:32 -0400

    added start menu fxml, not interactions yet

--- src/project/Client.java
+++ src/project/Client.java
@@ -1,5 +1,9 @@
 package project;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Client extends Application {
@@ -9,7 +13,26 @@ public class Client extends Application {
 	}
 
 	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Cooking Brain Game");
+	public void start(Stage primaryStage) throws Exception {
+		/**
+		 * root is set to start menu fxml file
+		 */
+		Parent root = 
+FXMLLoader.load(getClass().getResource("StartMenu.fxml"));
+		Scene scene = new Scene(root, 1280, 720);
+		
+		/*
+		 * window size can only be 1280x720 for now
+		 */
+		primaryStage.setTitle("Cooking Game");
+		primaryStage.setScene(scene);
+		primaryStage.setMinHeight(720);
+		primaryStage.setMinWidth(1280);
+		primaryStage.setMaxHeight(720);
+		primaryStage.setMaxWidth(1280);
+		primaryStage.show();
+		
+		
+		
 	}
 }
