beafe197c933c7ce605c0b58e06454537171eb68
cheungt1 <cheungt1@WIT45001318.wit.private>
2018-10-26 14:55:09 -0400

    changing title

--- src/project/Recipe_Book.java
+++ src/project/Recipe_Book.java
@@ -1,30 +1,5 @@
 package project;
 
-import java.util.Queue;
+public class Recipe_Book {
 
-/**
- * TESTING
- * @author Griffin Campbell, Martin Cheung, Sarah Kaczynski
- *
- * @param <K>
- * @param <V>
- */
-public class Recipe_Book<K extends RecipeLL_Node, V extends Queue<String>> implements IngredAndSteps<K,V> {
-	final private K key;
-	final private V value;
-
-	public Recipe_Book(K key, V value) {
-		this.key = key;
-		this.value = value;
-	}
-
-	@Override
-	public K getKey() {
-		return key;
-	}
-
-	@Override
-	public V getValue() {
-		return value;
-	}
 }
