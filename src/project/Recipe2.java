1a11eac054d45d39516dc7e364133ecbc5f1c22b
kaczynskis <kaczynskis@WIT45005445.wit.private>
2018-10-31 11:42:57 -0400

    added some ideas for recipe classes for easier organization

---
+++ src/project/Recipe2.java
@@ -0,0 +1,23 @@
+package project;
+
+public class Recipe2 {
+	public String ing1, ing2, ing3, ing4;
+	public Recipe2(String ing1, String ing2, String ing3, String ing4) {
+		this.ing1 = ing1;
+		this.ing2 = ing2;
+		this.ing3 = ing3;
+		this.ing4 = ing4;
+	}
+	public String getIng1() {
+		return ing1;
+	}
+	public String getIng2() {
+		return ing2;
+	}
+	public String getIng3() {
+		return ing3;
+	}
+	public String getIng4() {
+		return ing4;
+	}
+}
---
+++ src/project/Recipe2Star2.java
@@ -0,0 +1,11 @@
+package project;
+
+public class Recipe2Star2 extends Recipe2 {
+	public String ing1, ing2, ing3, ing4, ing5, ing6;
+	public Recipe2Star2(String ing1, String ing2, String ing3, String ing4, String ing5, String ing6) {
+		super(ing1, ing2, ing3, ing4);
+		this.ing5 = ing5;
+		this.ing6 = ing6;
+	}
+}
+
