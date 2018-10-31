31f9ece5e27339236565db52967131508ea1596c
cheungt1 <cheungt1@WIT45001318.wit.private>
2018-10-31 10:12:26 -0400

    minor changes

--- src/project/Recipe.java
+++ src/project/Recipe.java
@@ -5,18 +5,7 @@ package project;
  * 
  * @author Griffin Campbell, Martin Cheung, Sarah Kaczynski
  */
-public class Recipe {
-	/*
-	 * reference for using linked list
-	 * head is the first node 
-	 * tail is the last node
-	 * think of this as array,
-	 * 	instead of boxes together, it is a box then point to
-	 *  another box using "arrow" or references
-	 */
-	private static RecipeLL_Node head, tail;
-	private static myLinkedList<Ingredient> head2, tail2;
-
+public class Recipe {	
 	private static final Ingredient FLOUR = new Ingredient("Flour", "cup");
 	private static final Ingredient EGG = new Ingredient("Egg", "eggs");
 	private	static final Ingredient MILK = new Ingredient("Milk", "cup");
@@ -24,10 +13,13 @@ public class Recipe {
 	private static final Ingredient BUTTER = new Ingredient("Butter", "tbs");
 	private static final Ingredient SUGAR = new Ingredient("Sugar", "gram");
 
-	private static final String BAKE = "Bake";
-	private static final String MIX = "Mix";
-	private static final String CRACK = "Crack";
-	private static final String COOL = "Cool";
+	/*
+	 * private static final String BAKE = "Bake";
+	 * private static final String MIX = "Mix";
+	 * private static final String CRACK = "Crack";
+	 * private static final String COOL = "Cool";
+	 */
+
 	/**
 	 * create a ingredient list(LL) for cake
 	 */
@@ -44,94 +36,27 @@ public class Recipe {
 		CHOCOLATE.setAmount(2);
 		BUTTER.setAmount(3);
 		SUGAR.setAmount(4);
-		
-		/*
-		 * setting the first node of the linked list
-		 * when there is only one node. the head and tail is the 
-		 * same "box"
-		 */
-		head = new RecipeLL_Node(new Ingredient("Cake"));
-		tail = head;
-		
-		/*
-		 * tail is pointed to a new ingredient
-		 * then that new ingredient becomes the new tail of 
-		 * the linked list
-		 * 
+
+		/* 
 		 * think as connecting marker together, adding a marker to the end
 		 * then the new marker is the new end rinse and repeat
 		 */
-		tail.setNext(tail = new RecipeLL_Node(FLOUR));
-		tail.setNext(tail = new RecipeLL_Node(EGG));
-		tail.setNext(tail = new RecipeLL_Node(MILK));
-		tail.setNext(tail = new RecipeLL_Node(CHOCOLATE));
-		tail.setNext(tail = new RecipeLL_Node(BUTTER));
-		tail.setNext(tail = new RecipeLL_Node(SUGAR));
+		myLinkedList<Ingredient> ig = new myLinkedList<Ingredient>();
 		
+		ig.addLast(new myLinkedList<Ingredient>(new Ingredient("Cake")));
+		ig.addLast(new myLinkedList<Ingredient>(FLOUR));
+		ig.addLast(new myLinkedList<Ingredient>(EGG));
+		ig.addLast(new myLinkedList<Ingredient>(MILK));
+		ig.addLast(new myLinkedList<Ingredient>(CHOCOLATE));
+		ig.addLast(new myLinkedList<Ingredient>(BUTTER));
+		ig.addLast(new myLinkedList<Ingredient>(SUGAR));
+
 		//Queue<String> q = new Queue<String>
-		
-		head2 = new myLinkedList<Ingredient>(new Ingredient("Cake"));
-		tail2 = head2;
-		tail2.setNext(tail2 = new myLinkedList<Ingredient>(FLOUR));
-		tail2.setNext(tail2 = new myLinkedList<Ingredient>(EGG));
-		tail2.setNext(tail2 = new myLinkedList<Ingredient>(MILK));
-		tail2.setNext(tail2 = new myLinkedList<Ingredient>(CHOCOLATE));
-		tail2.setNext(tail2 = new myLinkedList<Ingredient>(BUTTER));
-		tail2.setNext(tail2 = new myLinkedList<Ingredient>(SUGAR));
-		
-		printRLL(head); // make sure it works
-		printMLL(head2);
+
+
+		System.out.println(ig.printList());
 	}
-	
-	/**
-	 * print the linked list to the console
-	 * for checking to see if the recipe is correct
-	 * @param head of a linked list
-	 */
-	private static void printRLL(RecipeLL_Node h) {
-		String name = "";
-		String ingredient = "";
-		
-		/*
-		 * if there is a head, there is a linked list
-		 * the first node is the name of the recipe
-		 */
-		if(h != null) {
-			name = String.format("%s:", h.getIngred().getName());
-		}
-		
-		/*
-		 * looping through a linked list, not using a while-loop
-		 * similar to regular for-loop, the element to start, where to end 
-		 * and how to move the reference/pointer
-		 * 
-		 * this is the part to show the ingredient 
-		 */
-		for(RecipeLL_Node n = h.getNext(); n != null; n = n.getNext()) {
-			ingredient += String.format("%s ", n.toString());
-		}
-		
-		//print to console
-		System.out.format("%s%n%-5s%s", name, "", ingredient);
-		System.out.println();
-	}
-	
-	private static <T> void printMLL(myLinkedList<Ingredient> h) {
-		String name = "";
-		String ingredient = "";
-		
-	
-		if(h != null) {
-			name = String.format("%s:", h.getInfo().getName());
-		}
-		
-		for(myLinkedList<Ingredient> n = h.getNext(); n != null; n = n.getNext()) {
-			ingredient += String.format("%s ", n.toString());
-		}
-	
-		System.out.format("%s%n%-5s%s", name, "", ingredient);
-	}
-	
+
 	/**
 	 * test each recipe
 	 * @param args command-line, ignored
