977f76de9d66c5da74400d7961ff065b22ff5564
cheungt1 <cheungt1@WIT45001318.wit.private>
2018-11-02 10:18:01 -0400

    added recipes

--- src/project/Recipe.java
+++ src/project/Recipe.java
@@ -8,17 +8,6 @@ package project;
 public class Recipe {	
 	private static Recipe_Book<Recipe_Pair<myLinkedList<Ingredient>, myQueue<String>>> recipeBook = new Recipe_Book<>();
 
-	private static final Ingredient FLOUR = new Ingredient("Flour", "cup");
-	private static final Ingredient EGG = new Ingredient("Egg", "eggs");
-	private	static final Ingredient MILK = new Ingredient("Milk", "cup");
-	private static final Ingredient CHOCOLATE = new Ingredient("Chocolate", "cup");
-	private static final Ingredient BUTTER = new Ingredient("Butter", "tbs");
-	private static final Ingredient SUGAR = new Ingredient("Sugar", "gram");
-	private static final Ingredient BAKING_MIX = new Ingredient("Baking Mix", "cup");
-	private static final Ingredient OIL = new Ingredient("Oil", "tsp");
-	private static final Ingredient FROSTING = new Ingredient("Frosting", "cup");
-	private static final Ingredient YEAST = new Ingredient("Yeast", "gram");
-
 	private static final String BAKE = "Bake";
 	private static final String MIX = "Mix";
 	private static final String CRACK = "Crack";
@@ -27,24 +16,22 @@ public class Recipe {
 	private static final String FROST = "Frost";
 	private static final String STIR = "Stir";
 	private static final String SHAPE = "Shape";
+	private static final String ROLL = "Roll";
+	private static final String ADD= "Add";
+	private static final String MASH = "Mash";
+	private static final String POUR = "Pour";
+	private static final String YEAST = "Feed Yeast";
+	private static final String MILK = "Milk";
+	private static final String RISE = "Let Rise";
+	private static final String PAN = "Place in Pan";
+	private static final String COCO = "Chocolate Chips";
+	private static final String SPRINKLES = "Sprinkles";
 
 
 	/**
 	 * create a ingredient list(LL) for cake
 	 */
 	private static void createChocolateCake() {
-		/*
-		 * each recipe uses similar ingredient
-		 * the only difference is the amount of each
-		 * therefore create the ingredient object, static final,
-		 * then set the amount for each recipe
-		 */
-		FLOUR.setAmount(3);
-		EGG.setAmount(2);
-		MILK.setAmount(4);
-		CHOCOLATE.setAmount(2);
-		BUTTER.setAmount(3);
-		SUGAR.setAmount(4);
 
 		/* 
 		 * think as connecting marker together, adding a marker to the end
@@ -54,12 +41,12 @@ public class Recipe {
 		myLinkedList<Ingredient> cakeIG = new myLinkedList<Ingredient>();
 
 		cakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Cake")));
-		cakeIG.addLast(new myLinkedList<Ingredient>(FLOUR));
-		cakeIG.addLast(new myLinkedList<Ingredient>(EGG));
-		cakeIG.addLast(new myLinkedList<Ingredient>(MILK));
-		cakeIG.addLast(new myLinkedList<Ingredient>(CHOCOLATE));
-		cakeIG.addLast(new myLinkedList<Ingredient>(BUTTER));
-		cakeIG.addLast(new myLinkedList<Ingredient>(SUGAR));
+		cakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("FLOUR")));
+		cakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("EGG")));
+		cakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("MILK")));
+		cakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("COCO")));
+		cakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("BUTTER")));
+		cakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("SUGAR")));
 
 		System.out.println(cakeIG.head.toString() + ": ");
 		System.out.println(cakeIG.printList());
@@ -70,83 +57,143 @@ public class Recipe {
 		System.out.println(cakeIG.getSize());
 
 		/////////////////// Queue Test \\\\\\\\\\\\\\\\\\\\
-		myQueue<String> cakeIN = new myQueue<String>();
+		myQueue<String> cakeIS = new myQueue<String>();
 
-		cakeIN.add(CRACK);
-		cakeIN.add(MIX);
-		cakeIN.add(BAKE);
-		cakeIN.add(COOL);
+		cakeIS.add(CRACK);
+		cakeIS.add(MIX);
+		cakeIS.add(BAKE);
+		cakeIS.add(COOL);
 
-		System.out.println(cakeIN.printQueue());
-		System.out.println(cakeIN.peek());
-		System.out.println(cakeIN.isEmpty());
-		System.out.println(cakeIN.size());
+		System.out.println(cakeIS.printQueue());
+		System.out.println(cakeIS.peek());
+		System.out.println(cakeIS.isEmpty());
+		System.out.println(cakeIS.size());
 
 		/////////////////// Recipe_Book Test \\\\\\\\\\\\\\\\\\\\		
-		recipeBook.add(new Recipe_Pair<myLinkedList<Ingredient>, myQueue<String>>(cakeIG, cakeIN));
-		recipeBook.add(cakeIG, cakeIN);		
-	}
-
-	private static void createBread() {
-		FLOUR.setAmount(7);
-		EGG.setAmount(3);
-		MILK.setAmount(4);
-		BUTTER.setAmount(3);
-		SUGAR.setAmount(4);
-
-		myLinkedList<Ingredient> breadIG = new myLinkedList<Ingredient>();
-
-		breadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Bread")));
-		breadIG.addLast(new myLinkedList<Ingredient>(FLOUR));
-		breadIG.addLast(new myLinkedList<Ingredient>(EGG));
-		breadIG.addLast(new myLinkedList<Ingredient>(MILK));
-		breadIG.addLast(new myLinkedList<Ingredient>(CHOCOLATE));
-		breadIG.addLast(new myLinkedList<Ingredient>(BUTTER));
-		breadIG.addLast(new myLinkedList<Ingredient>(SUGAR));
-
-		myQueue<String> breadIN = new myQueue<String>();
-
-		breadIN.add(CRACK);
-		breadIN.add(MIX);
-		breadIN.add(BAKE);
-		breadIN.add(COOL);
-
-		recipeBook.add(breadIG, breadIN);
+		recipeBook.add(new Recipe_Pair<myLinkedList<Ingredient>, myQueue<String>>(cakeIG, cakeIS));
+		recipeBook.add(cakeIG, cakeIS);		
 	}
 
 	/**
 	 * create sugar cookies
 	 */
 	private static void createSugarCookies() {
+		myLinkedList<Ingredient> sugarCookiesIG = new myLinkedList<>();
 
+		sugarCookiesIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Sugar Cookies")));
+		sugarCookiesIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Butter")));
+		sugarCookiesIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Sugar")));
+		sugarCookiesIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Flour")));
+		sugarCookiesIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Egg")));
+
+		myQueue<String> sugarCookiesIS = new myQueue<>();
+
+		sugarCookiesIS.add(CREAM);
+		sugarCookiesIS.add(STIR);
+		sugarCookiesIS.add(ROLL);
+		sugarCookiesIS.add(BAKE);
+
+		recipeBook.add(sugarCookiesIG, sugarCookiesIS);
 	}
-	
+
 	/**
 	 * create chocolate chip cookies
 	 */
 	private static void createChocolateChipCookies() {
+		myLinkedList<Ingredient> chocolateCakeIG = new myLinkedList<>();
 
+		chocolateCakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Chocolate Chip Cookie")));
+		chocolateCakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Butter")));
+		chocolateCakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Sugar")));
+		chocolateCakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Flour")));
+		chocolateCakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Egg")));
+		chocolateCakeIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Chocolate Chips")));
+
+		myQueue<String> chocolateCakeIS = new myQueue<>();
+
+		chocolateCakeIS.add(CREAM);
+		chocolateCakeIS.add(STIR);
+		chocolateCakeIS.add(ADD + COCO);
+		chocolateCakeIS.add(SHAPE);
+		chocolateCakeIS.add(BAKE);
+
+		recipeBook.add(chocolateCakeIG, chocolateCakeIS);
 	}
-	
+
 	/**
 	 * create banana bread
 	 */
 	private static void createBananaBread() {
+		myLinkedList<Ingredient> bananaBreadIG = new myLinkedList<>();
 
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Banana Bread")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Banana")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Flour")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Sugar")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Egg")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Cinnamon")));
+
+		myQueue<String> bananaBreadIS = new myQueue<>();
+
+		bananaBreadIS.add(MASH);
+		bananaBreadIS.add(MIX);
+		bananaBreadIS.add(POUR);
+		bananaBreadIS.add(BAKE);
+		bananaBreadIS.add(COOL);
+
+		recipeBook.add(bananaBreadIG, bananaBreadIS);
 	}
-	
+
 	/**
 	 * create white bread
 	 */
 	private static void createWhiteBread() {
+		myLinkedList<Ingredient> whiteBreadIG = new myLinkedList<>();
 
+		whiteBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("White Bread")));
+		whiteBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Yeast")));
+		whiteBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Flour")));
+		whiteBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Sugar")));
+		whiteBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Water")));
+		whiteBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Salt")));
+		whiteBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Butter")));
+
+		myQueue<String> whiteBreadIS = new myQueue<>();
+
+		whiteBreadIS.add(YEAST);
+		whiteBreadIS.add(ADD + MILK);
+		whiteBreadIS.add(RISE);
+		whiteBreadIS.add(PAN);
+		whiteBreadIS.add(RISE);
+		whiteBreadIS.add(BAKE);
+
+		recipeBook.add(whiteBreadIG, whiteBreadIS);
 	}
-	
+
 	/**
 	 * create fancy cake
 	 */
 	private static void createFancyCakee() {
+		myLinkedList<Ingredient> bananaBreadIG = new myLinkedList<>();
 
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Fancy Cake")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Flour")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Butter")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Sugar")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Egg")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Frosting")));
+		bananaBreadIG.addLast(new myLinkedList<Ingredient>(new Ingredient("Sprinkles")));
+
+		myQueue<String> bananaBreadIS = new myQueue<>();
+
+		bananaBreadIS.add(CREAM);
+		bananaBreadIS.add(STIR);
+		bananaBreadIS.add(BAKE);
+		bananaBreadIS.add(COOL);
+		bananaBreadIS.add(FROST);
+		bananaBreadIS.add(ADD + SPRINKLES);
+
+		recipeBook.add(bananaBreadIG, bananaBreadIS);
 	}
 
 	/**
@@ -155,7 +202,7 @@ public class Recipe {
 	 */
 	public static void main(String[] args) {
 		createChocolateCake();
-		createBread();
+
 		createSugarCookies();
 		createChocolateChipCookies();
 		createBananaBread();
