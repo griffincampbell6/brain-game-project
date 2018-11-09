a0adefb78464bb94e170b9cab03f820945500ad5
campbellg4 <campbellg4@WIT45005734.wit.private>
2018-11-09 11:57:29 -0500

    Changed recipes menu and controller

---
+++ src/project/RecipeController.java
@@ -0,0 +1,26 @@
+package project;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+
+public class RecipeController implements Initializable {
+	@FXML private Button SugarCookiesBtn;
+	@FXML private Button ChocolateCakeBtn;
+	@FXML private Button ChocolateCookiesBtn;
+	@FXML private Button BananaBreadBtn;
+	@FXML private Button BreadWhiteBtn;
+	@FXML private Button FancyCakeBtn;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+	}
+
+}
+ 
