36acebce6ab287b35ec8b02d4d80ba1f946ff1ba
campbellg4 <campbellg4@WIT45005734.wit.private>
2018-11-09 12:36:56 -0500

    add button id's to controller, and recipemenu fxml changes

--- src/project/RecipeController.java
+++ src/project/RecipeController.java
@@ -1,24 +1,43 @@
 package project;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 public class RecipeController implements Initializable {
-	@FXML private Button SugarCookiesBtn;
-	@FXML private Button ChocolateCakeBtn;
-	@FXML private Button ChocolateCookiesBtn;
-	@FXML private Button BananaBreadBtn;
-	@FXML private Button BreadWhiteBtn;
-	@FXML private Button FancyCakeBtn;
+	@FXML private Button sugarCookiesBtn;
+	@FXML private Button chocolateCakeBtn;
+	@FXML private Button chocolateCookiesBtn;
+	@FXML private Button bananaBreadBtn;
+	@FXML private Button breadWhiteBtn;
+	@FXML private Button fancyCakeBtn;
+	
+	@FXML private Pane sugarCookiesPane;
+	@FXML private Pane chocolateCakePane;
+	@FXML private Pane chocolateCookiesPane;
+	@FXML private Pane bananaBreadPane;
+	@FXML private Pane breadWhitePane;
+	@FXML private Pane fancyCakePane;
+	
+	@FXML private ImageView recipeImage;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
+		sugarCookiesBtn.setOnMouseEntered((event -> {
+			sugarCookiesPane.setStyle("-fx-background-color: #00FFFF");
+			
+		}));
+		sugarCookiesBtn.setOnMouseExited((event -> {
+			sugarCookiesPane.setStyle("-fx-backgroun-color: #ffffff");
+		}));
 		
 	}
 
