5a3b910befac6579032416f1282f2d2604cd2132
campbellg4 <campbellg4@WIT45005734.wit.private>
2018-11-10 11:26:04 -0500

    recipe menu layout and controller changes

--- src/project/RecipeController.java
+++ src/project/RecipeController.java
@@ -16,14 +16,14 @@ public class RecipeController implements Initializable {
 	@FXML private Button chocolateCakeBtn;
 	@FXML private Button chocolateCookiesBtn;
 	@FXML private Button bananaBreadBtn;
-	@FXML private Button breadWhiteBtn;
+	@FXML private Button whiteBreadBtn;
 	@FXML private Button fancyCakeBtn;
 	
 	@FXML private Pane sugarCookiesPane;
 	@FXML private Pane chocolateCakePane;
 	@FXML private Pane chocolateCookiesPane;
 	@FXML private Pane bananaBreadPane;
-	@FXML private Pane breadWhitePane;
+	@FXML private Pane whiteBreadPane;
 	@FXML private Pane fancyCakePane;
 	
 	@FXML private ImageView recipeImage;
@@ -32,7 +32,7 @@ public class RecipeController implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
 		/*
-		 * SUGAR COOKIE BUTTON EVENT
+		 * SUGAR COOKIE BUTTON EVENTS
 		 */
 		sugarCookiesBtn.setOnMouseEntered((event -> {
 			sugarCookiesPane.setStyle("-fx-background-color: #00FFFF");
@@ -45,44 +45,78 @@ public class RecipeController implements Initializable {
 				e.printStackTrace();
 			}
 		}));
-		
-		/*
-		 * SUGAR COOKKIE BUTTON EVENT
-		 */
 		sugarCookiesBtn.setOnMouseExited((event -> {
 			sugarCookiesPane.setStyle("-fx-backgroun-color: #ffffff");
 			recipeImage.setImage(null);
 		}));
+		sugarCookiesBtn.setOnAction((event -> {
+			//TODO
+		}));
 		
 		/*
-		 * CHOCHOLATE CAKE BUTTON EVENT
+		 * CHOCHOLATE CAKE BUTTON EVENTS
 		 */
 		chocolateCakeBtn.setOnMouseEntered((event -> {
-			
+			chocolateCakePane.setStyle("-fx-background-color: #00FFFF");
 		}));
-		
-		/*
-		 * CHOCHOLATE CAKE BUTTON EVENT
-		 */
 		chocolateCakeBtn.setOnMouseExited((event -> {
-			
+			chocolateCakePane.setStyle("-fx-backgroun-color: #ffffff");
+		}));
+		chocolateCakeBtn.setOnAction((event -> {
+			//TODO
 		}));
 		
 		/*
-		 * CHOCOLATE CHIP COOKIES BUTTON EVENT
+		 * CHOCOLATE CHIP COOKIES BUTTON EVENTS
 		 */
 		chocolateCookiesBtn.setOnMouseEntered((event -> {
-			
+			chocolateCookiesPane.setStyle("-fx-background-color: #00FFFF");
+		}));
+		chocolateCookiesBtn.setOnMouseExited((event -> {
+			chocolateCookiesPane.setStyle("-fx-backgroun-color: #ffffff");
+		}));
+		chocolateCookiesBtn.setOnAction((event -> {
+			//TODO
 		}));
 		
 		/*
-		 * CHOCOLATE CHIP COOKIE BUTTON EVENT
+		 * BANANA BREAD BUTTON EVENTS
 		 */
-		chocolateCookiesBtn.setOnMouseExited((event -> {
-			
+		bananaBreadBtn.setOnMouseEntered((event -> {
+			bananaBreadPane.setStyle("-fx-background-color: #00FFFF");
+		}));
+		bananaBreadBtn.setOnMouseExited((event -> {
+			bananaBreadPane.setStyle("-fx-backgroun-color: #ffffff");
+		}));
+		bananaBreadBtn.setOnAction((event -> {
+			//TODO
 		}));
 		
+		/*
+		 * WHITE BREAD BUTTON EVENTS
+		 */
+		whiteBreadBtn.setOnMouseEntered((event -> {
+			whiteBreadPane.setStyle("-fx-background-color: #00FFFF");
+		}));
+		whiteBreadBtn.setOnMouseExited((event -> {
+			whiteBreadPane.setStyle("-fx-backgroun-color: #ffffff");
+		}));
+		whiteBreadBtn.setOnAction((event -> {
+			//TODO
+		}));
 		
+		/*
+		 * FANCY CAKE BUTTON EVENTS
+		 */
+		fancyCakeBtn.setOnMouseEntered((event -> {
+			fancyCakePane.setStyle("-fx-background-color: #00FFFF");
+		}));
+		fancyCakeBtn.setOnMouseExited((event -> {
+			fancyCakePane.setStyle("-fx-backgroun-color: #ffffff");
+		}));
+		fancyCakeBtn.setOnAction((event -> {
+			//TODO
+		}));
 	}
 
 }
