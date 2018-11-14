90979f401abfaee42fb2566b7daea8872c612f86
campbellg4 <campbellg4@WIT45005734.wit.private>
2018-11-13 19:32:05 -0500

    added "hover to show details" to difficulty menu
    when hovering over pane of difficulty a text box is revealed with
    details on that difficulty

--- src/project/DifficultyMenuController.java
+++ src/project/DifficultyMenuController.java
@@ -11,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class DifficultyMenuController implements Initializable {
@@ -19,9 +20,22 @@ public class DifficultyMenuController implements Initializable {
 	@FXML private Button hardBtn;
 	@FXML private Button backBtn;
 	
+	@FXML private Pane easyBtnPane;
+	@FXML private Pane mediumBtnPane;
+	@FXML private Pane hardBtnPane;
+	
 	@FXML private Pane easyPane;
 	@FXML private Pane mediumPane;
 	@FXML private Pane hardPane;
+	
+	@FXML private Text easyHover;
+	@FXML private Text mediumHover;
+	@FXML private Text hardHover;
+	
+	@FXML private Text easyDetails;
+	@FXML private Text mediumDetails;
+	@FXML private Text hardDetails;
+	
 	
 	private static String START_MENU = "StartMenu.fxml";
 	private static String RECIPES_MENU = "RecipesMenu.fxml";
@@ -48,45 +62,66 @@ public class DifficultyMenuController implements Initializable {
 		}));
 		
 		/*
-		 * EASY BUTTON EVENT
+		 * EASY BUTTON EVENTS
 		 * loads recipes menu
+		 * reveals difficulty details
 		 * TODO add functionality to time given
 		 */
+		easyPane.setOnMouseEntered((event -> {
+			fullPaneEnter(easyHover, easyDetails);
+		}));
+		easyPane.setOnMouseExited((event -> {
+			fullPaneExit(easyHover, easyDetails);
+		}));
 		easyBtn.setOnMouseEntered((event -> {
-			paneEnter(easyPane);
+			ButtonPaneEnter(easyBtnPane);
 		}));
 		easyBtn.setOnMouseExited((event -> {
-			paneExit(easyPane);
+			buttonPaneExit(easyBtnPane);
 		}));
 		easyBtn.setOnAction((event -> {
 			loadNext();
 		}));
 		
 		/*
-		 * MEDIUM BUTTON EVENT
+		 * MEDIUM BUTTON EVENTS
 		 * loads recipes menu
+		 * reveals difficulty details
 		 * TODO add functionality to time given
 		 */
+		mediumPane.setOnMouseEntered((event -> {
+			fullPaneEnter(mediumHover, mediumDetails);
+		}));
+		mediumPane.setOnMouseExited((event -> {
+			fullPaneExit(mediumHover, mediumDetails);
+		}));
 		mediumBtn.setOnMouseEntered((event -> {
-			paneEnter(mediumPane);
+			ButtonPaneEnter(mediumBtnPane);
 		}));
 		mediumBtn.setOnMouseExited((event -> {
-			paneExit(mediumPane);
+			buttonPaneExit(mediumBtnPane);
 		}));
 		mediumBtn.setOnAction((event -> {
 			loadNext();
 		}));
 		
 		/*
-		 * HARD BUTTON EVENT
+		 * HARD BUTTON EVENTS
 		 * loads recipes menu
+		 * reveals difficulty details
 		 * TODO add functionality to time given
 		 */
+		hardPane.setOnMouseEntered((event -> {
+			fullPaneEnter(hardHover, hardDetails);
+		}));
+		hardPane.setOnMouseExited((event -> {
+			fullPaneExit(hardHover, hardDetails);
+		}));
 		hardBtn.setOnMouseEntered((event -> {
-			paneEnter(hardPane);
+			ButtonPaneEnter(hardBtnPane);
 		}));
 		hardBtn.setOnMouseExited((event -> {
-			paneExit(hardPane);
+			buttonPaneExit(hardBtnPane);
 		}));
 		hardBtn.setOnAction((event -> {
 			loadNext();
@@ -99,19 +134,41 @@ public class DifficultyMenuController implements Initializable {
 	 * mouse enter
 	 * @param pane of button
 	 */
-	private void paneEnter(Pane pane) {
+	private void ButtonPaneEnter(Pane pane) {
 		pane.setStyle("-fx-background-color: #00FFFF;"
 				+ "-fx-border-color: #000000");
 	}
 	
-	/**method ti styke pabes with
+	/**method to styke pabes with
 	 * mouse exit
 	 * 
 	 * @param pane of button
 	 */
-	private void paneExit(Pane pane) {
+	private void buttonPaneExit(Pane pane) {
 		pane.setStyle("-fx-background-color: #ffffff;"
 				+ "-fx-border-color: #000000");
+	}
+	
+	/**
+	 * method to reveal difficulty detail text box
+	 * and hid "hover here" text box
+	 * @param hover
+	 * @param details
+	 */
+	private void fullPaneEnter(Text hover, Text details) {
+		hover.setStyle("-fx-opacity: 0");
+		details.setStyle("-fx-opacity: 1");
+	}
+	
+	/**
+	 * method to reveal "hover here" text box 
+	 * and hive difficulty detail text box
+	 * @param hover
+	 * @param details
+	 */
+	private void fullPaneExit(Text hover, Text details){
+		hover.setStyle("-fx-opacity: 1");
+		details.setStyle("-fx-opacity: 0");
 	}
 	
 	/**
