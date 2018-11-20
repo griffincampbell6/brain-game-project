a2cffae218eaf9c869d1d480fc85a425260fb679
cheungt1 <cheungt1@WIT45001318.wit.private>
2018-11-20 16:18:24 -0500

    initial commit

--- src/project/Recipe2Tester.java
+++
@@ -1,8 +0,0 @@
-package project;
-
-public class Recipe2Tester {
-	public static void main(String[] args) {
-		Recipe2Star2 medium = new Recipe2Star2("butter", "flour", "sugar", "eggs", "milk", "chocolate chips");
-		System.out.println(medium.getIng1() + " " + medium.getIng2() + " " + medium.getIng3() + " " + medium.getIng4() + " " + medium.ing5 + " " + medium.ing6);
-	}
-}
