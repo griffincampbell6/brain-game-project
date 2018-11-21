dcad7507d53b6948f9da987a2626ee9ebdd038f3
cheungt1 <cheungt1@WIT45001318.wit.private>
2018-11-21 11:49:46 -0500

    changes littler stuff to timer, still no lbl update

--- src/project/IngredientDisplayController.java
+++ src/project/IngredientDisplayController.java
@@ -2,6 +2,9 @@ package project;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -27,26 +30,27 @@ public class IngredientDisplayController {
 			@Override
 			public void run() {
 				String[] a = RecipeController.curArray;
-				
+
 				for(int i = 0; i < a.length; i++) {
 					System.out.println(a[i]);
 				}
-				
+
 				Ing1.setText(a[0]);
 				Ing2.setText(a[1]);
 				Ing3.setText(a[2]);
 				Ing4.setText(a[3]);
 				Ing5.setText(a[4]);
 				Ing6.setText(a[5]);
-				
+
 				System.exit(0); // will change to other function
 			}
 		};
 
+
 		Timer timer = new Timer();
 		long delay = 5000;
-		long intevalPeriod = 5000;
-		
-		timer.scheduleAtFixedRate(task, delay, intevalPeriod);	
+
+		timer.schedule(task, delay);	
+
 	}
 }
